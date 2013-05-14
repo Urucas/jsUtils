7ef000ad4e96feaaad2153e6b244b22587c7276e
Bruno Alassia <vruno.alassia@gmail.com>
2013-05-14 17:26:55 -0300

    Jquery extension to center elements on a screen

--- utils.js
+++ utils.js
@@ -17,3 +17,13 @@ function getURLVars(urlString) {
 	});
     return vars;
 }
+
+// jquery extension to center an element into the screen
+jQuery.fn.center = function () {
+    this.css("position","absolute");
+    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + 
+                                                $(window).scrollTop()) + "px");
+    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + 
+                                                $(window).scrollLeft()) + "px");
+    return this;
+}
